85ae7d6326d62c5715d3cb9327581d7c8c11f649
VU DUC TRI JOSEPH <vutrilkvn89@gmail.com>
2025-07-26 11:11:53 +0700

    fist lock : rsc , use client, layout, connection betweens SC and CC, loading

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,21 @@
-export default function Page() {
-  return <h1>Cabin page</h1>;
+import Counter from "../Component/Counter";
+
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/1/posts");
+
+  const data = await res.json();
+
+  return (
+    <div>
+      <h1>Cabins page</h1>
+      <ul>
+        {data.map((el) => (
+          <li key={el.id}>{el.title}</li>
+        ))}
+      </ul>
+      <Counter />
+    </div>
+  );
 }
+
+// "https://jsonplaceholder.typicode.com/users/1/posts"
--- app/page.js
+++ app/page.js
@@ -1,11 +1,7 @@
-import Link from "next/link";
-import Navigation from "./Component/Navigation";
-
 export default function Page() {
   return (
     <div>
       <h1>The Wild Oasis. Welcome to paradise</h1>;
-      <Navigation />
     </div>
   );
 }
