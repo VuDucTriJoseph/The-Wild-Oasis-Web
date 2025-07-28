ebd16ee8b59a02efcd9c1883b52101af8d10c077
VU DUC TRI JOSEPH <vutrilkvn89@gmail.com>
2025-07-28 14:44:46 +0700

    all done for layout and rount

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,4 +1,7 @@
-import ReservationCard from "./ReservationCard";
+import ReservationCard from "@/app/_components/ReservationCard";
+export const metadata = {
+  title: "Reservations",
+};
 
 export default function Page() {
   // CHANGE
@@ -6,9 +9,7 @@ export default function Page() {
 
   return (
     <div>
-      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Your reservations
-      </h2>
+      <h2 className="font-semibold text-2xl text-accent-400 mb-7">Your reservations</h2>
 
       {bookings.length === 0 ? (
         <p className="text-lg">
--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,33 @@
-import Logo from "./Component/Logo";
-import Navigation from "./Component/Navigation";
+import Header from "./_components/Header";
+
+import { Josefin_Sans } from "next/font/google";
+import "@/app/_style/globals.css";
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+// console.log(josefin);
 
 export const metadata = {
-  title: "The Wild Oasis",
+  // title: "The Wild Oasis",
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located i the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by Joseph Vu</footer>
+      <body
+        className={`bg-primary-950 text-gray-50 antialiased min-h-screen ${josefin.className} flex flex-col relative`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
       </body>
     </html>
   );
