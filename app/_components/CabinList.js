024fd7c6cf5e5987237e08e8a73f4f29772c8f94
VU DUC TRI JOSEPH <vutrilkvn89@gmail.com>
2025-07-30 11:10:02 +0700

    add serch button feartur in cabins page (sharing state between client and server used URL) add server componet in client component

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -2,15 +2,26 @@ import { unstable_noStore as noStore } from "next/cache";
 import CabinCard from "./CabinCard";
 import { getCabins } from "../_lib/data-service";
 
-async function CabinList() {
+async function CabinList({ filter }) {
   noStore();
   const cabins = await getCabins();
 
   if (!cabins.length) return null;
 
+  let displayCanbins;
+
+  if (filter === "all") displayCanbins = cabins;
+
+  if (filter === "small") displayCanbins = cabins.filter((cabin) => cabin.maxCapacity <= 3);
+
+  if (filter === "medium")
+    displayCanbins = cabins.filter((cabin) => cabin.maxCapacity > 3 && cabin.maxCapacity < 8);
+
+  if (filter === "large") displayCanbins = cabins.filter((cabin) => cabin.maxCapacity >= 8);
+
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {displayCanbins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
