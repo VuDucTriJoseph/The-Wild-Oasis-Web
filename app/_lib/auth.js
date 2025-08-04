bb62da8f21ca7a18323f92b8adc0f8ddead5a52f
VU DUC TRI JOSEPH <vutrilkvn89@gmail.com>
2025-08-04 10:51:04 +0700

    create Login page to navigate to protect the account page from the unsigned guest. make a new fist account for first guest , and mutetion by guest id

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
+import { se } from "date-fns/locale";
 
 const authConfig = {
   providers: [
@@ -9,6 +11,34 @@ const authConfig = {
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
+  callbacks: {
+    authorized({ auth, request }) {
+      return !!auth?.user;
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest) {
+          createGuest({
+            email: user.email,
+            fullName: user.name,
+          });
+          return true;
+        }
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestID = guest.id;
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
 };
 
 export const {
--- middleware.js
+++ middleware.js
@@ -1,7 +1,14 @@
-import { NextResponse } from "next/server";
+import { auth } from "./app/_lib/auth";
+
+/*import { NextResponse } from "next/server";
 
 export function middleware(request) {
   //   console.log(request);
 
   return NextResponse.redirect(new URL("/about", request.url));
-}
+}*/
+export const middleware = auth;
+
+export const config = {
+  matcher: ["/account"],
+};
